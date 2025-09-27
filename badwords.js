7f5db782c0eb594f666d903b650122caa6200287
Mohimanu2 <93042936+Mohimanu2@users.noreply.github.com>
2025-09-27 15:35:54 +0600

    Update badwords.js

--- badwords.js
+++ badwords.js
@@ -1,5 +1,23 @@
 module.exports = [
-  "madarchod","chutiya","maaderchod",       // Banglish
-  "মাদারচোদ","চুদ","চুদে",                 // Bangla
-  "fuck","bastard","bal"                    // English
+  // English
+  "fuck","bitch","bastard","asshole","dick","piss","cunt","slut","whore","shit","damn","crap",
+  "bollocks","bugger","cock","wanker","prick","twat","arse","motherfucker","nigger","fag",
+  "douche","retard","idiot","moron","screw","screw you","jerk","bloody","darn","hell","pussy",
+  "dumb","stupid","loser","freak","ass","cockhead","twit","shithead","tool","wimp","chode",
+
+  // Banglish
+  "madarchod","chutiya","maaderchod","choda","boka","lokh","bokachoda","chud","maderchod",
+  "chudai","bokachod","maachoda","chudbe","chodai","chudbo","madarchodi","madarchod","chuder",
+  "chudina","chuderami","bokar","bokachoda","madarchoder","chudchhe","chudlo","madar","chudina",
+  "chudeci","maaderchod","chudbi","chudbe","chudbe","chudar","chudli","chudk","chudchi","chudlo",
+  "magi","maagi",   // Banglish for “মাগি”
+
+  // Bangla
+  "মাদারচোদ","চুদ","চুদে","বোকা","চোদা","মাদারচোদ","চোদাই","চুদবী","চুদবে","চুদলি",
+  "মাদারচোদে","চোদ","মাদারচোদী","চোদবী","চোদবি","বোকাচোদ","চোদলাম","চুদবী","চুদবো",
+  "চুদেছি","মাদেরচোদ","চুদব","চোদেছি","চুদছি","মাদারচোদ","চুদবু","চোদবু","চুদলি","চুদছি",
+  "মাগি",   // Bangla
+
+  // Extra mild/common swear words
+  "bastard","damn","crap","shit","darn","idiot","loser","stupid","freak","wimp","jerk","asshole"
 ];
