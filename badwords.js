e4011894238ffed5e9d04634c5c47f78f7808bbf
Mohimanu2 <93042936+Mohimanu2@users.noreply.github.com>
2025-09-27 15:32:07 +0600

    Create badwords.js

---
+++ badwords.js
@@ -0,0 +1,5 @@
+module.exports = [
+  "madarchod","chutiya","maaderchod",       // Banglish
+  "মাদারচোদ","চুদ","চুদে",                 // Bangla
+  "fuck","bastard","bal"                    // English
+];
